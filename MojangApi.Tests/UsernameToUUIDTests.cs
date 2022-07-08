0899b3ace0eca2d074e1bd284e0784b38d5c537f
Feng Chang <13818037+chang196700@users.noreply.github.com>
2022-07-08 14:47:14 +0800

    Update UsernameToUUIDTests.cs

--- MojangApi.Tests/UsernameToUUIDTests.cs
+++ MojangApi.Tests/UsernameToUUIDTests.cs
@@ -9,7 +9,7 @@ namespace MojangApi.Tests
         [Fact]
         public async System.Threading.Tasks.Task RequestRawTestAsync()
         {
-            var request = new UsernameToUUIDRequest("chang196700");
+            var request = new UsernameToUUIDRequest("MomoYoki");
             var resp = await ClientHelper.RequestRawAsync(request);
             var content = await resp.Content.ReadAsStringAsync();
             Assert.NotEmpty(content);
@@ -19,7 +19,7 @@ namespace MojangApi.Tests
         [Fact]
         public async System.Threading.Tasks.Task RequestTestAsync()
         {
-            var request = new UsernameToUUIDRequest("chang196700");
+            var request = new UsernameToUUIDRequest("MomoYoki");
             var resp = await ClientHelper.RequestAsync(request);
             Assert.True(resp.IsSuccess);
             Assert.Equal("00992bb84f6949b990dc309476a1426b", resp.Content.Id);
